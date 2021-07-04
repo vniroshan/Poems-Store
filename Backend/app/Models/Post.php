05b0293d2b16de0dcbf15a75bf3e8bb032ae556d
niroshan <48372147+vniroshan@users.noreply.github.com>
2021-07-04 07:28:30 +0530

    Frontend Added

---
+++ Backend/app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Like;
+use App\Models\User;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'user_id',
+        'image'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
---
+++ frontend/android/app/src/main/kotlin/com/niroshan/frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.niroshan.frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
